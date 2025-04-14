84d6c3bae658203139bc1fb72326bd1c723900ed
Mateas Bogdan <mateas.bogdan@gmail.com>
2025-04-15 00:10:56 +0300

    Adaugat footerul, legalele si buymeacoffee <3

--- src/App.js
+++ src/App.js
@@ -13,6 +13,10 @@ import AdminPanel from './pages/AdminPanel';
 import PendingMemes from './pages/PendingMemes';
 import UserDetails from './pages/UserDetails';
 import MyMemes from './pages/MyMemes';
+import Terms from './pages/Terms';
+import Privacy from './pages/Privacy';
+import Disclaimer from './pages/Disclaimer';
+import Contact from './pages/Contact';
 import { AuthProvider } from './contexts/AuthContext';
 import { NotificationProvider } from './components/common/Notification';
 import VerifyEmail from './components/auth/VerifyEmail';
@@ -37,6 +41,10 @@ function App() {
                 <Route path="/pending" element={<PendingMemes />} />
                 <Route path="/mymemes" element={<MyMemes />} />
                 <Route path="/verify-email" element={<VerifyEmail />} />
+                <Route path="/terms" element={<Terms />} />
+                <Route path="/privacy" element={<Privacy />} />
+                <Route path="/disclaimer" element={<Disclaimer />} />
+                <Route path="/contact" element={<Contact />} />
                 <Route path="*" element={<NotFound />} />
               </Routes>
             </main>
